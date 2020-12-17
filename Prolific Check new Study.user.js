e4468d5a18f7945845e7b7a41992342ecc35f95f
F-Wer <43503689+F-Wer@users.noreply.github.com>
2020-12-17 19:10:39 +0100

    Added the functionality to send a request to localhost if there is a new study. Therefore I wrote a small python program with the server functioanlity and the function to send a e-mail. (if you want it's not that hard to implement twillio for sms)

--- Prolific Check new Study.user.js
+++ Prolific Check new Study.user.js
@@ -5,9 +5,12 @@
 // @description  Check if there is a new study and notify.
 // @author       https://github.com/F-Wer/
 // @include      https://app.prolific.co/studies
+// @icon             https://app.prolific.co/favicon.ico
 // @grant        none
 // ==/UserScript==
+
 var url = 'https://app.prolific.co/studies';
+const request = 'http://localhost:8000/';
 
 
 window.onload = function(){
@@ -20,26 +23,28 @@ window.onload = function(){
 ;
 
 function prolificcallSite(){
+//     Possibility to get the count of elements with a specific class name. Problem -> if there is one or none the length starts at 0.
+//   var classlength = document.getElementsByClassName('#detail-wrap').length;
+//     console.log(classlength);
 
-    var blankSite = document.getElementsByClassName('studies-list unselected');
-    // if(typeof blankSite === 'undefined') {
-     var n = '';
-         for(n=0; n<10;n++) {
-    var text = document.getElementsByClassName('detail-wrap')[n].innerText;
-
-
-    console.log(text);
-             var notification = new Notification('NEW STUDY' ,{body: text});
-
-     notification.onclick = function(event) {
-         event.preventDefault();
-         window.open(url, '_blank');
-         }
-
-     }
-return;
+var innertext = document.getElementsByClassName('detail-wrap')[0];
+    var loremipsum = 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.';
+if(!innertext) {
+           console.log("There is no new study");
+           return;
+       }else {
+    for(var n=0; n < 3 ; n++) {
+        var text = document.getElementsByClassName('detail-wrap')[n].innerText;
+       var notification = new Notification('NEW STUDY' ,{body: text});
+            calltoServer(text);
+       notification.onclick = function(event) {
+           event.preventDefault();
+           window.open(url, '_blank');
+       };
+    }
+    return;
 }
-
+}
 
 function randomfunccall() {
     window.setInterval(function(){
@@ -47,3 +52,11 @@ function randomfunccall() {
     }, 60000);
 }
 
+function calltoServer (text) {
+ var xhr = new XMLHttpRequest();
+xhr.open("POST", request, true);
+xhr.setRequestHeader('Content-Type', 'text/html');
+xhr.send(JSON.stringify(
+    text
+));
+}
